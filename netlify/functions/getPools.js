cbb549ef3c03873df038c71c64384434f16a8622
jpwoodi <81716774+jpwoodi@users.noreply.github.com>
2024-08-20 16:41:46 +0100

    Update getPools.js

--- netlify/functions/getPools.js
+++ netlify/functions/getPools.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
-    const baseId = 'appHlKACTnR2r7DGk'; // Your Airtable Base ID
+    const baseId = 'process.env.AIRTABLE_BASE_ID'; // Your Airtable Base ID
     const token = process.env.AIRTABLE_TOKEN;    // Ensure this is set in your Netlify environment variables
     const tableName = 'SwimmingPools';           // Your Airtable Table Name
 
