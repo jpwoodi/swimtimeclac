1887ae9db3598328c67265bd2758d49b52e00a41
jpwoodi <81716774+jpwoodi@users.noreply.github.com>
2024-08-20 15:52:38 +0100

    Update getPools.js

--- netlify/functions/getPools.js
+++ netlify/functions/getPools.js
@@ -4,7 +4,7 @@ exports.handler = async function(event, context) {
     const baseId = process.env.AIRTABLE_BASE_ID;
     const token = process.env.AIRTABLE_TOKEN;
 
-    const url = `https://api.airtable.com/v0/${baseId}/your_table_name`;
+    const url = `https://api.airtable.com/v0/${baseId}/SwimmingPools`;
     const response = await fetch(url, {
         headers: {
             Authorization: `Bearer ${token}`
@@ -12,6 +12,8 @@ exports.handler = async function(event, context) {
     });
 
     const data = await response.json();
+
+    console.log('Airtable response:', JSON.stringify(data, null, 2)); // Log the response to check its structure
 
     return {
         statusCode: 200,
