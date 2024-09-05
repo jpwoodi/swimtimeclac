05cf5659bbb4156086097086abf05b69404e0261
jpwoodi <81716774+jpwoodi@users.noreply.github.com>
2024-09-05 15:44:30 +0100

    Update get-strava-client-id.js

--- netlify/functions/get-strava-client-id.js
+++ netlify/functions/get-strava-client-id.js
@@ -1,8 +1,9 @@
 exports.handler = async (event, context) => {
     const clientId = process.env.CLIENT_ID_STRAVA;
+    const redirectUri = process.env.REDIRECT_URI_STRAVA; // Make sure this is set
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ clientId }),
+        body: JSON.stringify({ clientId, redirectUri }),
     };
 };
